c3f5674d9fe2f232e07dfaef2e68cf48732545b2
JThilinaDK123 <JThilinaDK123@gmail.com>
2025-09-15 00:40:38 +0530

    second commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name = "MLOps-Project-01",
+    version = "1.0",
+    author = "Thilina",
+    packages = find_packages(),
+    install_requires = requirements
+)
+
+
+
